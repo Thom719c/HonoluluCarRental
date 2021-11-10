283f76e460783c39903c2a45aec732b7c5cc99d2
Thom719c <thom719c@stud.kea.dk>
2021-11-10 11:53:47 +0100

    Made Switch case

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,7 +1,12 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class FileProcessor {
-    public static void updateArrayList(ArrayList<Car>cars) {
-
+    public static void updateArrayList(ArrayList<Car>cars) throws FileNotFoundException {
+        Scanner readCars = new Scanner(new File("Files/Cars"));
+        ArrayList<String> listTemp = new ArrayList<>();
+        while (readCars.hasNext()){
+            listTemp.add(String.valueOf(readCars.hasNextLine()));
+        }
     }
 }
