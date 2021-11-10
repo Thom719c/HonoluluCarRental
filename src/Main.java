dce76240bfb64ac521618e3e0ba6497d79d506eb
Thom719c <thom719c@stud.kea.dk>
2021-11-10 11:21:06 +0100

    Made Switch case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //arrayUpdate();
+        runHonoluluCarRentalProgram();
 
+    }
+    public static void runHonoluluCarRentalProgram(){
+        Scanner input = new Scanner(System.in);
+        int honoluluCarRentalChoice = 0;
+        while (honoluluCarRentalChoice != 3) {
+            System.out.println("Honolulu Car Rental menu" +
+                    "\n1. Cars \n2. Rentals \n3. For exit" + "\nIndtast nummer: ");
+            honoluluCarRentalChoice = input.nextInt();
+            input.nextLine();
+            switch (honoluluCarRentalChoice) {
+                case 1:
+                    cars();
+                    break;
+                case 2:
+                    rentals();
+                    break;
+                default:
+                    System.out.println();
+            }
+        }
+    }
+    public static void cars(){
+        Scanner input = new Scanner(System.in);
+        int carMenuChoice = 0;
+        while (carMenuChoice != 6) {
+            System.out.println("Car menu" +
+                    "\n1. Show Catalog \n2. Add car \n3. Remove car \n4. Search for car " +
+                    "\n5. Change info \n6. For exit" + "\nIndtast nummer: ");
+            carMenuChoice = input.nextInt();
+            input.nextLine();
+            switch (carMenuChoice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println();
+            }
+        }
+    }
+
+    public static void rentals(){
+        Scanner input = new Scanner(System.in);
+        int rentalChoice = 0;
+        while (rentalChoice != 6) {
+            System.out.println("Rental menu" +
+                    "\n1. Add private customer \n2. Add Company customer \n3. Create contract " +
+                    "\n4. Change info \n5. Search after contract \n6. For exit" + "\nIndtast nummer: ");
+            rentalChoice = input.nextInt();
+            input.nextLine();
+            switch (rentalChoice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    //Todo Should we overwrite and create new contract if there is wrong info (case 4 change info)
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println();
+            }
+        }
     }
 }
