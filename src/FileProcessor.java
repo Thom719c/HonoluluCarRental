e5616288c689d2b7d99fccd8436e938e2f55793d
Thom719c <thom719c@stud.kea.dk>
2021-11-12 15:51:30 +0100

    AddFamily - guy

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -3,20 +3,21 @@ import java.util.*;
 
 public class FileProcessor {
     public static void updateArrayList(ArrayList<Car> cars) throws FileNotFoundException {
+
         Scanner readCars = new Scanner(new File("Files/Cars"));
         while (readCars.hasNextLine()) {
             String[] carInfo = readCars.nextLine().split(", ");
-            if (readCars.equals("Family")) { // carInfo[0].equals("Family")
+            if (carInfo[0].equals("Family")) { // carInfo[0].equals("Family")
                 //Car familyCar = new Family("Family", brand, model, fuelType, registration, odometer, manualGear, airCondition, cruiseControl, seat);
-                Car familyCar = new Family("Family", carInfo[0], carInfo[1], carInfo[2], carInfo[3], Integer.parseInt(carInfo[4]), carInfo[5], carInfo[6], carInfo[7], Integer.parseInt(carInfo[8]));
+                Car familyCar = new Family("Family", carInfo[1], carInfo[2], carInfo[3], carInfo[4], Integer.parseInt(carInfo[5]), carInfo[6], carInfo[7], carInfo[8], Integer.parseInt(carInfo[9]));
                 cars.add(familyCar);
             } else if (readCars.equals("Luxury")) {
                 //Car luxuryCar = new Luxury("Luxury", brand, model, fuelType, registration, odometer, ccm, automaticGear, airCondition, cruiseControl, leatherSeat);
-                Car luxuryCar = new Luxury("Luxury", carInfo[0], carInfo[1], carInfo[2], carInfo[3], Integer.parseInt(carInfo[4]), Integer.parseInt(carInfo[5]), carInfo[6], carInfo[7], carInfo[8], carInfo[9]);
+                Car luxuryCar = new Luxury("Luxury", carInfo[1], carInfo[2], carInfo[3], carInfo[4], Integer.parseInt(carInfo[5]), Integer.parseInt(carInfo[6]), carInfo[7], carInfo[8], carInfo[9], carInfo[10]);
                 cars.add(luxuryCar);
             } else if (readCars.equals("Sport")) {
                 //Car sportCar = new Sport("Luxury", brand, model, fuelType, registration, odometer, manualGear, hp);
-                Car sportCar = new Sport("Sport", carInfo[0], carInfo[1], carInfo[2], carInfo[3], Integer.parseInt(carInfo[4]), carInfo[5], Integer.parseInt(carInfo[6]));
+                Car sportCar = new Sport("Sport", carInfo[1], carInfo[2], carInfo[3], carInfo[4], Integer.parseInt(carInfo[5]), carInfo[6], Integer.parseInt(carInfo[7]));
                 cars.add(sportCar);
             }
         }
@@ -28,7 +29,7 @@ public class FileProcessor {
         BufferedWriter bWrite = new BufferedWriter(writeFile);
 
         String carsOut = "";
-        
+
         for (int i = 0; i < cars.size(); ++i) {
             if (cars.get(i).type.equals("Family")) {
                 System.out.println(cars.get(i));
