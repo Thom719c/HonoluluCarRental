8f4786d10b71a121a237b0dbeda7ba0dea4371e3
Thom719c <thom719c@stud.kea.dk>
2021-11-16 15:48:06 +0100

    ContractHandler

--- src/ContractHandler.java
+++ src/ContractHandler.java
@@ -2,8 +2,25 @@ import java.util.*;
 
 public class ContractHandler {
     private CarHandler carHandler = new CarHandler();
+    private RentalsHandler rentalsHandler = new RentalsHandler();
 
     public void contractHandler(Scanner input){
+        createContract(input);
+    }
+    public void createContract(Scanner input){
+        System.out.println("Enter Renter ID: ");
+        int renterId = input.nextInt();
+        System.out.println("Enter Company ID: ");
+        int companyId = input.nextInt();
+        System.out.println("Enter contract length: ");
+        String contractLength = input.nextLine();
+        System.out.println("Enter start km: ");
+        int startKm = input.nextInt();
+        System.out.println("Enter max km: ");
+        int maxKm = input.nextInt();
+        System.out.println("Enter registration number: ");
+        int registrationNumber = input.nextInt();
 
+        Rental_Contract rental_contract = new Rental_Contract(renterId, companyId, contractLength, startKm, maxKm, registrationNumber);
     }
 }
